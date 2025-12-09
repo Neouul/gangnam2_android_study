883bbf270617166fd4d1093c54b1d256904431df
Neouul <fluctu0q0@gmail.com>
2025-12-09 17:26:34 +0900

    feat: Application 재정의

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/AppApplication.kt
@@ -0,0 +1,10 @@
+package com.survivalcoding.gangnam2kiandroidstudy
+
+import android.app.Application
+
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
