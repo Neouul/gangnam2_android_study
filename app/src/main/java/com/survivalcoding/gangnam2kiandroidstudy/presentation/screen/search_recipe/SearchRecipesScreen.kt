6177b2f379f20dff4d77c21f587658e0993b22c1
Neouul <fluctu0q0@gmail.com>
2025-12-11 18:01:17 +0900

    chore: SearchRecipesScreen의 Preview 작성

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipe/SearchRecipesScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipe/SearchRecipesScreen.kt
@@ -1,35 +1,29 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.search_recipe
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.data.model.Recipe
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.SearchBar
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.SmallRecipeCard
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
@@ -73,61 +67,12 @@ fun SearchRecipesScreen(
             )
         }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(46.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            OutlinedTextField(
-                value = state.searchTerm,
-                modifier = Modifier
-                    .weight(1f)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(color = AppColors.white),
-                placeholder = {
-                    Text(
-                        text = "Search recipe",
-                        color = AppColors.gray4,
-                        style = AppTextStyles.smallerTextRegular
-                    )
-                },
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = AppColors.gray4,
-                    unfocusedBorderColor = AppColors.gray4,
-                ),
-                shape = RoundedCornerShape(10.dp),
-                onValueChange = onSearchTermChange,
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(R.drawable.outline_search_normal),
-                        contentDescription = "검색 아이콘",
-                        modifier = Modifier.size(20.dp),
-                        tint = AppColors.gray4,
-                    )
-                },
-            )
-
-            Spacer(Modifier.width(20.dp))
-
-            // 필터 아이콘
-            Box(
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(color = AppColors.primary100)
-                    .clickable { onFilterClick() }
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.outline_setting_4),
-                    contentDescription = "필터 아이콘",
-                    tint = AppColors.white,
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .size(20.dp)
-                )
-            }
-        }
+        // 검색란
+        SearchBar(
+            state = state,
+            onSearchTermChange = onSearchTermChange,
+            onFilterClick = onFilterClick,
+        )
 
         // Search 타이틀
         Box(
@@ -185,4 +130,25 @@ fun LoadingOverlay() {
             color = AppColors.gray3
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewSearchRecipesScreen() {
+    SearchRecipesScreen(
+        state = SearchRecipesState(
+            allRecipes = listOf(
+                Recipe(
+                    id = 5,
+                    category = "American",
+                    name = "Grilled salmon with avocado salsa",
+                    imageUrl = "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
+                    chef = "Alice Johnson",
+                    time = "25 min",
+                    rating = 4.5,
+                    ingredients = listOf()
+                ),
+            )
+        )
+    )
 }
