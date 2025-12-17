2a14adf9da94114c2b5ec7116f1dfa73dc9f669c
Neouul <fluctu0q0@gmail.com>
2025-12-17 10:27:13 +0900

    feat: Clean Architecture 연습문제 1 - UseCase를 Root에서 주입

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipe/SavedRecipesRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipe/SavedRecipesRoot.kt
@@ -6,18 +6,24 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.survivalcoding.gangnam2kiandroidstudy.AppApplication
+import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.MockRecipeDataSourceImpl
+import com.survivalcoding.gangnam2kiandroidstudy.data.repository.MockBookmarkRepositoryImpl
+import com.survivalcoding.gangnam2kiandroidstudy.data.repository.RecipeRepositoryImpl
+import com.survivalcoding.gangnam2kiandroidstudy.domain.usecase.GetSavedRecipesUseCase
 
 @Composable
 fun SavedRecipesRoot(
     onCardClick: (Long) -> Unit,
     viewModel: SavedRecipesViewModel = viewModel(
         factory = SavedRecipesViewModel.factory(
-            LocalContext.current.applicationContext as AppApplication
-        )
+            application = LocalContext.current.applicationContext as AppApplication,
+            useCase = GetSavedRecipesUseCase(bookmarkRepository = MockBookmarkRepositoryImpl(), recipeRepository =  RecipeRepositoryImpl(
+                MockRecipeDataSourceImpl())),
+        ),
     ),
 ) {
-
     val state by viewModel.state.collectAsStateWithLifecycle()
+
     SavedRecipesScreen(
         state = state,
         onCardClick = { recipeId -> onCardClick(recipeId) },
